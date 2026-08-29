b514dd33704fd5ca8dd15f5c7abbf5941dc84d5e
ars <ars28022011@icloud.com>
2026-08-29 11:11:40 +0300

    урок3

--- клиент.py
+++ клиент.py
@@ -1,5 +1,37 @@
+import math
 import socket
 import time
+import pygame
 mainsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mainsocket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 mainsocket.connect(('localhost',10000))
+pygame.init()
+width=800
+height=600
+СС=(width//2,height//2)
+old=[0,0]
+radius=50
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption('бактерия')
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+        if pygame.mouse.get_focused():
+            pos=pygame.mouse.get_pos()
+            vector=pos[0]-СС[0],pos[1]-СС[1]
+            lenv=math.sqrt(vector[0]**2+vector[1]**2)
+            vector=vector[0]/lenv,vector[1]/lenv
+            if lenv<=radius:
+                vector=0,0
+            if vector!=old:
+                old=vector
+                msg=f'<{vector[0]},{vector[1]}>'
+                mainsocket.send(msg.encode())
+    data=mainsocket.recv(1024).decode()
+    print(data)
+    screen.fill('white')
+    pygame.draw.circle(screen,'green',СС,radius)
+    pygame.display.update()
+pygame.quit()
--- сервер.py
+++ сервер.py
@@ -1,5 +1,94 @@
 import socket
 import time
+import pygame
+import psycopg2
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import declarative_base,sessionmaker
+engine=create_engine('postgresql+psycopg2://postgres:543098@localhost/бактерия')
+Session=sessionmaker(engine)
+Base=declarative_base()
+s=Session()
+pygame.init()
+WIDTHROOM,HEIGHTROOM=4000,4000
+WIDTHSERVER,HEIGHTSERVER=300,300
+FPS=100
+screen=pygame.display.set_mode((WIDTHSERVER,HEIGHTSERVER))
+pygame.display.set_caption('server')
+clock=pygame.time.Clock()
+class Player(Base):
+    __tablename__='gamers'
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    name=Column(String)
+    addres=Column(String)
+    x=Column(Integer,default=500)
+    y=Column(Integer,default=500)
+    size=Column(Integer,default=50)
+    errors=Column(Integer,default=0)
+    abs_speed=Column(Integer,default=1)
+    speed_x=Column(Integer,default=0)
+    speed_y=Column(Integer,default=0)
+    def __init__(self,name,address):
+        self.name=name
+        self.address=address
+Base.metadata.create_all(engine)
+class LocalPlayer():
+   def __init__(self,id,name,sock,address):
+       self.id=id
+       self.db=s.get(Player,self.id)
+       self.name=name
+       self.sock=sock
+       self.address=address
+       self.x=500
+       self.y=500
+       self.size=50
+       self.errors=0
+       self.abs_speed=1
+       self.speed_x=0
+       self.speed_y=0
 mainsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mainsocket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 mainsocket.bind(('localhost',10000))
+mainsocket.setblocking(False)
+mainsocket.listen(5)
+players={}
+run=True
+while run:
+    clock.tick(FPS)
+    try:
+        newsocket,addr=mainsocket.accept()
+        print('подключился',addr)
+        newsocket.setblocking(False)
+        player=Player('name',addr)
+        s.merge(player)
+        s.commit()
+        addr=f'({addr[0]},{addr[1]})'
+        data=s.query(Player).filter(Player.addres==addr)
+        for user in data:
+            localPlayer=LocalPlayer(user.id,'name',newsocket,addr)
+            players[user.id]=localPlayer
+    except:
+        pass
+    for id in list(players):
+        try:
+            data=players[id].sock.recv(1024).decode()
+            print(data)
+        except:
+            pass
+    for id in list(players):
+        try:
+            players[id].sock.send('hi'.encode())
+        except:
+            players[id].sock.close()
+            del players[id]
+            s.query(Player).filter(Player.id==id).delete()
+            s.commit()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+
+
+
+pygame.quit()
+mainsocket.close()
+s.query(Player).delete()
+s.commit()
